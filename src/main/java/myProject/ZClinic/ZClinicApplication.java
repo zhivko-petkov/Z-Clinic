993fa62b962d15a6adda05fadd90044e69e9c3b2
Zhivko Petkov <zhivko258@gmail.com>
2022-07-25 01:00:05 +0300

    Initial commit.

---
+++ src/main/java/myProject/ZClinic/ZClinicApplication.java
@@ -0,0 +1,13 @@
+package myProject.ZClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZClinicApplication.class, args);
+	}
+
+}
