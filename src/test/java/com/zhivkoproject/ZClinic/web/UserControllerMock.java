b18881b0e492a95ba188d5181c0233899e243683
Zhivko Petkov <zhivko258@gmail.com>
2022-08-03 15:50:31 +0300

    Finish with Register and Login user tests.

--- src/test/java/com/zhivkoproject/ZClinic/web/UserControllerMock.java
+++ src/test/java/com/zhivkoproject/ZClinic/web/UserControllerMock.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -52,8 +54,52 @@ public class UserControllerMock {
                         param("additionalDetails", "She is a famous star.").
                         param("imageUrl", "https://bit.ly/3oN6btc")
                         .with(csrf())
-                ).andExpect(redirectedUrl("/"));
+                ).andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+        assertEquals(1, userRepository.count());
+
     }
+
+    @Test
+    public void registerFailPostShownTest() throws Exception {
+        mockMvc.perform(post("/users/register").
+                        param("username", "").
+                        param("firstName", "Tonka").
+                        param("surname", "Koleva").
+                        param("email", "tonka@example.com").
+                        param("password", "topsecret").
+                        param("confirmPassword", "1").
+                        param("address", "Sofia").
+                        param("additionalDetails", "She is a famous star.").
+                        param("imageUrl", "https://bit.ly/3oN6btc")
+                        .with(csrf())
+                ).andExpect(redirectedUrl("/users/register"));
+        assertEquals(0, userRepository.count());
+    }
+
+    @Test
+    public void testUserLogin() throws Exception {
+        mockMvc.perform(post("/users/register").
+                param("username", "tonka").
+                param("firstName", "Tonka").
+                param("surname", "Koleva").
+                param("email", "tonka@example.com").
+                param("password", "topsecret").
+                param("confirmPassword", "topsecret").
+                param("address", "Sofia").
+                param("additionalDetails", "She is a famous star.").
+                param("imageUrl", "https://bit.ly/3oN6btc")
+                .with(csrf())
+        );
+        mockMvc.perform(post("/users/login").
+                param("username", "tonka").
+                param("password", "topsecret")
+                .with(csrf())
+        ).andExpect(redirectedUrl("/"));
+        assertEquals(1, userRepository.count());
+    }
+
+
 
 
 
