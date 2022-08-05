062f092a98c7977de030e300996c8f0dbf07e529
Zhivko Petkov <zhivko258@gmail.com>
2022-08-06 00:29:38 +0300

    Solve bug with IpAddress interceptor.

--- src/main/java/com/zhivkoproject/ZClinic/interceptor/IpBlackListInterceptor.java
+++ src/main/java/com/zhivkoproject/ZClinic/interceptor/IpBlackListInterceptor.java
@@ -4,6 +4,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,36 +16,50 @@ public class IpBlackListInterceptor implements HandlerInterceptor {
     private final Map<String, LocalTime> startActivityTime = new HashMap<>();
 
     private final LocalTime localTime = LocalTime.now();
-    private List<String> blacklistedIpAddresses = new ArrayList<>();
+    private final List<String> blacklistedIpAddresses = new ArrayList<>();
 
     public IpBlackListInterceptor() {
         //this.blacklistedIpAddresses.add("0:0:0:0:0:0:0:1");
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
         String ipAddress = request.getRemoteAddr();
         if (ipActivity.containsKey(ipAddress)){
+            boolean isUpdate = false;
             int currentActivity = ipActivity.get(ipAddress);
 //time not working
-            //35-36
+            //начална страница - 28 пъти в рамките на една минута
+            //останалите страници - от 40 до 60 пъти в рамките на минута
             if (currentActivity > 250){
+
                 LocalTime userStartActivityTime = startActivityTime.get(ipAddress);
-                System.out.println(LocalTime.now().getMinute() - userStartActivityTime.getMinute());
-                if ((LocalTime.now().getMinute() - userStartActivityTime.getMinute()) <= 1){
+                Duration duration = Duration.between(userStartActivityTime, LocalTime.now());
+
+                if (duration.getSeconds() <= 60){
                     this.blacklistedIpAddresses.add(ipAddress);
+                    System.out.println(duration.getSeconds());
                 } else {
                     ipActivity.put(ipAddress, 1);
                     startActivityTime.put(ipAddress, LocalTime.now());
+                    isUpdate = true;
+                    System.out.println(LocalTime.now());
                 }
             }
-            currentActivity++;
-            ipActivity.put(ipAddress, currentActivity);
+
+            if (!isUpdate){
+                currentActivity++;
+                ipActivity.put(ipAddress, currentActivity);
+            }
+
         } else {
             ipActivity.put(ipAddress, 1);
             startActivityTime.put(ipAddress, LocalTime.now());
         }
         System.out.println(ipAddress);
+
         if (blacklistedIpAddresses.contains(ipAddress)) {
             response.sendError(400);
 
